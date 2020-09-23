02a05becfe18edf3fdafad66647f47640aa48637
Shahar s <cl_shaharshavit@rnd-hub.com>
2020-09-23 12:32:27 +0300

    feat(xyz): added bounding box support

--- Jmeter/google-xyz/xyz-timed.sh
+++ Jmeter/google-xyz/xyz-timed.sh
@@ -15,6 +15,12 @@ runTimeMin=1
 publishDb=imagery_test
 delayBetweenRequests=100 #ms
 
+#bounding box (must be epsg:4326)
+minX=-180
+minY=-90
+maxX=180
+maxY=90
+
 ## test script stat do not modify ###
 #preperations
 runTimeSec=$(($runTimeMin * 60))
@@ -23,7 +29,8 @@ dir="${resultsDir}/xyz/${now}"
 mkdir -p ${dir}
 #test
 ${jmeterBin}/jmeter -n -t ./xyz-timed.jmx -l ${dir}/xyz-res.jtl -JtargetHost=${host} -JtargetPort=${port} -Jusers=${users} -JrequestsPerUser=${userRequests} \
-        -JrunTimeSec=${runTimeSec} -Jlayer=${layer} -Jversion=${version} -JpublishDb=${publishDb} -JdelayBetweemRequestsMs=${delayBetweenRequests}
+        -JrunTimeSec=${runTimeSec} -Jlayer=${layer} -Jversion=${version} -JpublishDb=${publishDb} -JdelayBetweemRequestsMs=${delayBetweenRequests} \
+        -JminX=${minX} -JminY=${minY} -JmaxX=${maxX} -JmaxY=${maxY}
 #generate reports
 ${jmeterBin}/JMeterPluginsCMD.sh --generate-png ${dir}/xyz-test-rtot.png --generate-csv ${dir}/xyz-test-rtot.csv --input-jtl ${dir}/xyz-res.jtl --plugin-type ResponseTimesOverTime --width 800 --height 600
 ${jmeterBin}/JMeterPluginsCMD.sh --generate-csv ${dir}/xyz-test-agg.csv --input-jtl ${dir}/xyz-res.jtl --plugin-type AggregateReport
